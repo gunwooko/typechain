8737ca627f8a79af3bdd1f92b8fea328b58b23c6
gunwooko <kkw722@gmail.com>
2020-11-09 18:04:26 +0900

    my first Block 해결하지 못한 문제: TS에 prettier 적용

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,28 @@
-// interface Human { // interface는 TS에서만 실행 가능하다.
-//   name:string,
-//   age:number,
-//   gender:string
-// }
-
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string
-  constructor(name:string, age: number, gender:string){
-    this.name = name;
-    this.age = age;
-    this.gender = gender
-  }
+class Block {
+  public index: number
+  public hash: string
+  public previousHash: string
+  public data: string
+   public timestamp: number
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+    ){
+      this.index = index
+      this.hash = hash
+      this.previousHash = previousHash
+      this.data = data
+      this.timestamp = timestamp}
 }
 
-// const person ={
-//     name : "Gunwoo",
-//     age : 29, 
-//     gender : "male"
-// }
+const genesisBlock:Block = new Block(0, "0202020202", "", "Hello", 123456)
+const testBlock: Block = new Block(1, "0202020202", "", "World", 123456)
+// 여기서 blockchain 이란 블록의 연결이다. 이것을 배열로 표현할 수 있다. [Block] 이란 blockchain 안에는 Block class의 속성을 가진 것만 들어갈 수 있다. 
+let blockchain: [Block] = [genesisBlock] 
+// blockchain.push(testBlock)
 
-const Som = new Human('Som', 28, "female")
-
-const sayHi = (person: Human):string => {
-// 매개변수의 각 타입을 정해줄 수 있고, 또한 리턴값의 타입을 정해줄 수 있다. 이로써 더욱 선명한 코드를 작성할 수 있게 된다. 
-    return (`hello ${person.name}, you are ${person.age}, you are a ${person.gender}!!`)
- 
-}
-
-console.log(sayHi(Som))
-
+console.log(blockchain)
 export ={}
