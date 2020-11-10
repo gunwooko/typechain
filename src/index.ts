bcb8e44e9cfd681789ab326b121ef5825f396a96
gunwooko <kkw722@gmail.com>
2020-11-10 16:44:01 +0900

    Validating Block Structure

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 import * as CriptoJS from 'crypto-js';
 
 class Block {
-  public index: number;
-  public hash: string;
-  public previousHash: string;
-  public data: string;
-  public timestamp: number;
-
   // static method!
   static calculateBlockHash = (
     index: number,
@@ -15,6 +9,19 @@ class Block {
     data: string
   ): string =>
     CriptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+  static validateStructure = (aBlock: Block): boolean =>
+    typeof aBlock.index === 'number' &&
+    typeof aBlock.hash === 'string' &&
+    typeof aBlock.previousHash === 'string' &&
+    typeof aBlock.timestamp === 'number' &&
+    typeof aBlock.data === 'string';
+
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
   constructor(
     index: number,
@@ -31,11 +38,11 @@ class Block {
   }
 }
 
-const genesisBlock: Block = new Block(0, '0202020202', '', 'Hello', 123456);
-const testBlock: Block = new Block(1, '0202020202', '', 'World', 123456);
-// 여기서 blockchain 이란 블록의 연결이다. 이것을 배열로 표현할 수 있다. Block[]] 이란 blockchain 안에는 Block class의 속성을 가진 것만 들어갈 수 있다.
+const genesisBlock: Block = new Block(0, '0202020202', '', 'Genesis', 123456);
+// 여기서 blockchain 이란 블록의 연결이다. 이것을 배열로 표현할 수 있다. Block[] 이란 blockchain 안에는 Block class의 속성을 가진 것만 들어갈 수 있다.
 let blockchain: Block[] = [genesisBlock];
-blockchain.push(testBlock);
+// const testBlock: Block = new Block(1, '0202020202', '', 'World', 123456);
+// blockchain.push(testBlock);
 
 const getBlockchain = (): Block[] => blockchain;
 
@@ -43,4 +50,58 @@ const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
-export = {};
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  addBlock(newBlock);
+  return newBlock;
+};
+
+const getHashforBlock = (aBlock: Block): string =>
+  Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
+
+createNewBlock('first');
+createNewBlock('second');
+createNewBlock('third');
+console.log(getBlockchain());
+
+// export = {};
